d44f521cb9b5279c3fdbfa93a138f01158fd4b14
Ahmet Bolat <abolat01@gmail.com>
2019-09-20 18:17:12 -0400

    Update shares.py

--- shares.py
+++ shares.py
@@ -1,12 +1,13 @@
 #! usr/bin/env python3
-'''shares.py by Ahmet Bolat, 2019-25-06
+'''shares.py by Ahmet Bolat,
 This script will calculate your position size.
 '''
-
+#variables
 def main():
     equity = float(input('Enter your equity = '))
     price = float(input('Enter stock price = '))
-
+    
+#calculating share size
     if price < float(5) and equity < float(2500):
         print('Max position size for under $5 / under $2500 equity = ' + str(int(equity * float(2) / price)))
     elif price < float(5) and equity > float(2500):
