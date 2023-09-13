47cc734b203543d76c70ff07b8d627da25132006
Ahmet Bolat <dev.ahmet.bolat@gmail.com>
2023-09-13 19:26:51 +0300

    new_update

--- shares.py
+++ shares.py
@@ -1,21 +1,33 @@
-#! usr/bin/env python3
-'''shares.py by Ahmet Bolat,
-This script will calculate your position size.
-'''
-#variables
-def main():
-    equity = float(input('Enter your equity = '))
-    price = float(input('Enter stock price = '))
-    
-#calculating share size
-    if price < float(5) and equity < float(2500):
-        print('Max position size for under $5 / under $2500 equity = ' + str(int(equity * float(2) / price)))
-    elif price < float(5) and equity > float(2500):
-        print('Max position size for under $5 / over $2500 equity = ' + str(int(equity * float(2) / price)))
-    elif price > float(5) and equity < float(2500):
-        print('Max position size for over $5 / under $2500 equity = ' + str(int(equity * float(4) / price)))
+def calculate_position_size(equity, price):
+    if price < 5 and equity < 2500:
+        return int(equity * 2 / price)
+    elif price < 5 and equity >= 2500:
+        return int(equity * 2 / price)
+    elif price >= 5 and equity < 2500:
+        return int(equity * 4 / price)
     else:
-        print('Max position size for over $5 / over $2500 equity = ' + str(int(equity * float(6) / price)))
+        return int(equity * 6 / price)
+
+
+def main():
+    while True:
+        try:
+            equity = float(input('Enter your equity: $'))
+            price = float(input('Enter stock price: $'))
+
+            if equity < 0 or price < 0:
+                print("Both equity and price must be non-negative values.")
+            else:
+                position_size = calculate_position_size(equity, price)
+                print(f'Max position size: {position_size} shares')
+
+            response = input("Do you want to calculate again? (yes/no): ").strip().lower()
+            if response != "yes":
+                break
+        except ValueError:
+            print("Invalid input. Please enter valid numeric values for equity and stock price.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
